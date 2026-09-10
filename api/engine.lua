9a862115aea159fb5b870dc08237f9e4381f7a78
raybac <hponsin@proton.me>
2026-09-10 09:32:10 +0200

    changing api

--- api/engine.lua
+++ api/engine.lua
@@ -1,6 +1,6 @@
 ffi = require("ffi")
 
-ffi.cdef[[
+ffi.cdef [[
     // entity manager
     size_t entity_manager_allocate_entity_id(void);
     void entity_manager_free_id(size_t entity_id);
@@ -37,7 +37,7 @@ ffi.cdef[[
     // transform system
     void transform_system_attach_transform(size_t index);
     void transform_system_remove_transform(size_t index);
-    
+
     void transfrom_system_set_position(size_t index, Vec3 position);
     void transfrom_system_translate(size_t index, Vec3 vector);
     void transfrom_system_set_rotation(size_t index, Vec3 rotation);
@@ -60,18 +60,79 @@ ffi.cdef[[
 
 ]]
 
--- INPUT SYSTEM
+-- ENTITY MANAGER
+
+EntityManager = {}
+
+function EntityManager.newEntity() return ffi.C.entity_manager_allocate_entity_id() end
+
+function EntityManager.destroyEntity(entityId) return ffi.C.entity_manager_free_id(entityId) end
+
+-- SCRIPT SYSTEM
+
+ScriptSystem = {}
+
+function ScriptSystem.attachScript(entityId, path) return ffi.C.script_system_attach_script(entityId, path) end
+
+function ScriptSystem.removeScript(entityId) return ffi.C.script_system_remove_script(entityId) end
+
+function ScriptSystem.getScriptName(entityId) return ffi.C.script_system_get_script_name(entityId) end
+
+-- RESSOURCE MANAGER
+
+RessourceManager = {}
+
+function RessourceManager.acquireMesh(path) return ffi.C.ressource_manager_acquire_mesh(path) end
+
+function RessourceManager.releaseMesh(handle) return ffi.C.ressource_manager_release_mesh(handle) end
+
+-- TRANSFORM SYSTEM
+
+TransformSystem = {}
+
+function TransformSystem.attachTransform(entityId) return ffi.C.transform_system_attach_transform(entityId) end
+
+function TransformSystem.removeTransform(entityId) return ffi.C.transform_system_remove_transform(entityId) end
+
+function TransformSystem.setPosition(entityId, position) return ffi.C.transfrom_system_set_position(entityId, position) end
+
+function TransformSystem.translate(entityId, vector) return ffi.C.transfrom_system_translate(entityId, vector) end
+
+function TransformSystem.setRotation(entityId, rotation) return ffi.C.transfrom_system_set_rotation(entityId, rotation) end
+
+function TransformSystem.rotate(entityId, vector) return ffi.C.transfrom_system_rotate(entityId, vector) end
+
+function TransformSystem.setScale(entityId, scale) return ffi.C.transfrom_system_set_scale(entityId, scale) end
+
+function TransformSystem.scale(entityId, vector) return ffi.C.transfrom_system_scale(entityId, vector) end
 
 KeyCode = {
     Space = 44, W = 26, Z = 122, Q = 113, A = 4, S = 115, D = 100
 }
 
+-- RENDER SYSTEM
+
+RenderSystem = {}
+
+function RenderSystem.attachMesh(entityId, meshHandle) return ffi.C.render_system_attach_mesh(entityId, meshHandle) end
+
+function RenderSystem.removeMesh(entityId) return ffi.C.render_system_remove_mesh(entityId) end
+
+function RenderSystem.setCamera(entityId) return ffi.C.render_system_set_camera(entityId) end
+
+-- INPUT SYSTEM
+
 Input = {}
 function Input.GetKey(keycode) return ffi.C.input_is_held(keycode) end
+
 function Input.GetKeyDown(keycode) return ffi.C.input_is_pressed(keycode) end
+
 function Input.GetKeyUp(keycode) return ffi.C.input_is_released(keycode) end
+
 function Input.GetMousePosition() return ffi.C.input_get_mouse_position() end
+
 function Input.GetMouseDelta() return ffi.C.input_get_mouse_delta() end
+
 function Input.GetMouseScroll() return ffi.C.input_get_mouse_scroll() end
 
 -- GAMEOBJECT
